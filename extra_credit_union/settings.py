286d0ed3f2c2ee147e413f013e13b2a079d316ed
Rome Delmo <r.delmo314@gmail.com>
2026-05-01 11:40:59 +0100

    Fix for swagger

--- extra_credit_union/settings.py
+++ extra_credit_union/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',  # Add this line
     'django.middleware.common.CommonMiddleware',
